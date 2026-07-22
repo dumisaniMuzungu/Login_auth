c96772f9ac6ba170e5e6dac7e196c3bf3ed57876
dumisaniMuzungu <61735331+HashTagYmk@users.noreply.github.com>
2026-07-22 16:40:22 +0200

    test_1

---
+++ firebaseauth.js
@@ -0,0 +1,22 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAtc4dk85IkgFUVPyxNGsMH4dwoWCH2km0",
+    authDomain: "authentication-html-9e40e.firebaseapp.com",
+    projectId: "authentication-html-9e40e",
+    storageBucket: "authentication-html-9e40e.firebasestorage.app",
+    messagingSenderId: "210748274464",
+    appId: "1:210748274464:web:2ff9a7cfc018c2ace6c945",
+    measurementId: "G-08KRE7SE99"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
